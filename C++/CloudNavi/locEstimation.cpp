e9cdbdafbbc7e94f919f50361be93bb72708cff7
xiaoqiangteng <xteng99@gmail.com>
2018-05-04 15:48:38 +0800

    CloudNavi src

--- C++/CloudNavi/locEstimation.cpp
+++ C++/CloudNavi/locEstimation.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <time.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/registration/icp.h>
 #include <string>
@@ -29,7 +30,7 @@ using namespace std;
 void showHelp(char * program_name)
 {
     std::cout << std::endl;
-    std::cout << "Usage: " << program_name << " file_name[gt_file] file_name[est_file] path[gt_cloud] path[est_cloud] file_name[results]" << std::endl;
+    std::cout << "Usage: " << program_name << " file_name[gt_file] file_name[est_file] path[gt_cloud] path[est_cloud] file_name[results] file_name[result_all]" << std::endl;
     std::cout << "-h:  Show this help." << std::endl;
 }
 
@@ -88,10 +89,13 @@ int main (int argc, char** argv)
     string gt_cloud_path = argv[3];
     string est_cloud_path = argv[4];
     const char *results = argv[5];
+    const char *result_all = argv[6];
     //Write file
-    fstream outfile;
+    fstream outfile, outfile_all;
     outfile.open(results, ios::out);
-    outfile << "# est_cloud\tmost_gt_cloud\tcorrespondence_num\test_loc[0]\test_loc[1]\tgt_loc[0]\tgt_loc[1]" << endl;
+    outfile_all.open(result_all, ios::out);
+    outfile << "# est_cloud\tmost_gt_cloud\tcorrespondence_num\test_loc[0]\test_loc[1]\tgt_loc[0]\tgt_loc[1]\tcompute_time" << endl;
+    outfile_all << "# est_cloud\tgt_cloud\tcorrespondence_num\test_loc[0]\test_loc[1]\tgt_loc[0]\tgt_loc[1]\tload_time\tcompute_time" << endl;
     //Read the cloud name and the loc data of the gt and the est
     vector<string > gt_cloud_name_vec;
     vector<vector<string> > gt_loc_vec;
@@ -114,6 +118,7 @@ int main (int argc, char** argv)
 
     int total = int (est_cloud_name_vec.size()*gt_cloud_name_vec.size());
     int count = 0;
+    clock_t load_start_time_all, load_end_time_all, compute_start_time, compute_end_time, compute_start_time_all, compute_end_time_all;
     //Load point cloud data of the gt and the est
     for (int i = 0; i < est_cloud_name_vec.size(); ++i) {
         cout << "---------------------------------------------------------" << endl;
@@ -131,22 +136,29 @@ int main (int argc, char** argv)
         string most_similar_cloud;
         string most_similar_locX;
         string most_similar_locY;
+        compute_start_time = clock();
         for (int j = 0; j < gt_cloud_name_vec.size(); ++j) {
+            outfile_all << est_cloud_name_vec[i] << "\t";
+            outfile_all << gt_cloud_name_vec[j] << "\t";
             string gt_cloud_file = gt_cloud_path + gt_cloud_name_vec[j];
             cout << "[" << count*100/total << "%]" << "\tReading\t\t" << est_cloud_name_vec[i] << "\t" << gt_cloud_name_vec[j] << endl;
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZ>);
+            load_start_time_all = clock();
             if(pcl::io::loadPCDFile<pcl::PointXYZ> (gt_cloud_file, *cloud_out) == -1)
             {
                 PCL_ERROR ("Couldn't read file [gt_cloud_name_vec]");
                 return -1;
             }
+            load_end_time_all = clock();
             std::cout << "\t\tpoints: " << cloud_in->points.size () << "\t" << cloud_out->points.size () <<std::endl;
+            compute_start_time_all = clock();
             boost::shared_ptr<pcl::Correspondences> correspondences (new pcl::Correspondences);
             pcl::registration::CorrespondenceEstimation<pcl::PointXYZ, pcl::PointXYZ> corr_est;
             corr_est.setInputCloud (cloud_in);
             corr_est.setInputTarget (cloud_out);
             corr_est.determineCorrespondences (*correspondences,.1);
             std::cout << "\t\tCorrespondence: " << int (correspondences->size ()) << std::endl;
+            compute_end_time_all = clock();
 //            boost::shared_ptr<pcl::Correspondences> correspondences_remain (new pcl::Correspondences);
 //            pcl::registration::CorrespondenceRejectorMedianDistance rej;
 //            rej.setMedianFactor (8.79241104);
@@ -162,11 +174,18 @@ int main (int argc, char** argv)
             std::cout << "\t\tthe most similar cloud is " << most_similar_cloud << std::endl;
             std::cout << "\t\tcorrespondences number is " << most_similar_num << std::endl;
             count ++;
+            outfile_all << int (correspondences->size ()) << "\t" << est_locX_string << "\t"
+                    << est_locY_string << "\t" << gt_loc_vec[j][0] << "\t" << gt_loc_vec[j][1] << "\t"
+                    << (double) (load_end_time_all - load_start_time_all)/CLOCKS_PER_SEC << "\t"
+                    << (double) (compute_end_time_all - compute_start_time_all)/CLOCKS_PER_SEC << endl;
         }
+        compute_end_time = clock();
         outfile << most_similar_cloud << "\t" << most_similar_num << "\t" << est_locX_string << "\t"
-                << est_locY_string << "\t" << most_similar_locX << "\t" << most_similar_locY << endl;
+                << est_locY_string << "\t" << most_similar_locX << "\t" << most_similar_locY << "\t"
+                << (double) (compute_end_time - compute_start_time)/CLOCKS_PER_SEC << endl;
     }
     outfile.close();
+    outfile_all.close();
 
     return (0);
 }
